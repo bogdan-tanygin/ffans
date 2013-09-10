7ad12a11ce32ddb3edc5c4447661e3abce27a4e1
B.M.Tanygin@gmail.com <B.M.Tanygin@gmail.com@c73d5883-ad56-407f-ba61-18bf1b2862ed>
2013-09-10 21:18:43 +0000

    (F) Added parameters

--- trunk/modelsimcore.cpp
+++ trunk/modelsimcore.cpp
@@ -4,6 +4,7 @@
 #include <QObject>
 #include <QThread>
 #include <QDebug>
+#include <QSettings>
 
 void Worker::doWork(const QString &parameter){
     //double i = 0;
@@ -25,11 +26,31 @@ ModelSimCore::ModelSimCore()
 
 void ModelSimCore::modelSimInit(void)
 {
-    qDebug() << "Check fundamental constants init";
+    qDebug() << "Fundamental constants init";
     qDebug() << "--------------------------------";
-    qDebug() << "pi = " << pi;
-    qDebug() << "mu0 = " << mu0;
-    qDebug() << "Na = " << Na;
-    qDebug() << "kb = " << kb;
-    qDebug() << "R = " << R;
+    qDebug() << "pi =" << pi;
+    qDebug() << "mu0 =" << mu0;
+    qDebug() << "Na =" << Na;
+    qDebug() << "kb =" << kb;
+    qDebug() << "R =" << R;
+
+    qDebug() << "";
+    qDebug() << "Experimental setup";
+    qDebug() << "--------------------------------";
+    QSettings parameters(QString("parameters.ini"), QSettings::IniFormat);
+
+    double Lx = parameters.value("Vessel/Lx", "0").toDouble();
+    double Ly = parameters.value("Vessel/Ly", "0").toDouble();
+    double Lz = parameters.value("Vessel/Lz", "0").toDouble();
+    qDebug() << "Lx =" << Lx;
+    qDebug() << "Ly =" << Ly;
+    qDebug() << "Lz =" << Lz;
+
+    double T = parameters.value("Fluid/T", "0").toDouble();
+    qDebug() << "T =" << T;
+
+    double B1 = parameters.value("Field/B1", "0").toDouble();
+    double B2 = parameters.value("Field/B2", "0").toDouble();
+    qDebug() << "B1 =" << B1;
+    qDebug() << "B2 =" << B2;
 }
