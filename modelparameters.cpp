e94854f4e5bf1fbba5ffbe687fdb4fec26f99815
B.M.Tanygin@gmail.com <B.M.Tanygin@gmail.com@c73d5883-ad56-407f-ba61-18bf1b2862ed>
2013-09-15 19:26:24 +0000

    (Data) Parameters update (Tech) Refactoring of parameters processing (F) Added macroses for parameters processing and debug output

--- modelparameters.cpp
+++ modelparameters.cpp
@@ -1,3 +1,6 @@
+#include <QDebug>
+#include <QSettings>
+
 #include "modelparameters.h"
 
 // Fundamental constants [Metric System]
@@ -27,6 +30,52 @@ const double kb = 1.3806488 * 1E-23; // [m2 kg s-2 K-1]
 // Gas constant
 const double R = Na * kb; // [J K−1 mol−1]
 
-ModelParameters::ModelParameters()
+ModelParameters::ModelParameters(QString fileName)
 {
+    qDebug() << "Fundamental constants init";
+    qDebug() << "--------------------------------";
+    PWRITE(pi)
+    PWRITE(mu0)
+    PWRITE(Na)
+    PWRITE(kb)
+    PWRITE(R)
+
+    qDebug() << "";
+    qDebug() << "Experimental setup parameters";
+    qDebug() << "--------------------------------";
+    QSettings parameters(fileName, QSettings::IniFormat);
+
+    PREAD(Vessel, Lx, 1E-6) //[µm]
+    PREAD(Vessel, Ly, 1E-6) //[µm]
+    PREAD(Vessel, Lz, 1E-6) //[µm]
+
+    PREAD(Carrier fluid, rho_f, 1) //[kg m-3]
+    PREAD(Carrier fluid, t1, 1) // [°C]
+    PREAD(Carrier fluid, t2, 1) // [°C]
+
+    PREAD(Ferrofluid, d_mean, 1E-9) // [nm]
+    PREAD(Ferrofluid, s_mean, 1) // [A m2]
+
+    PREAD(Nanoparticle, m_mol_rel, 1) // []
+    PREAD(Nanoparticle, rho_p, 1) // [kg m-3]
+    PREAD(Nanoparticle, s_mol, muB) // [Bohr magnetons]
+    PREAD(Nanoparticle, Ms_p, 1E3) // [G]
+
+    qDebug() << "";
+    qDebug() << "Analysis";
+    qDebug() << "--------------------------------";
+
+    double V_particle_mean = pi * pow(d_mean, 3) / 6.0;
+    PWRITE(V_particle_mean)
+    double m_mean = rho_p * V_particle_mean;
+    PWRITE(m_mean)
+    double m_mol = m_mol_rel * 1E-3 / Na;
+    double N_mol = m_mean / m_mol;
+    PWRITE(N_mol)
+    double s_particle_mean = s_mol * N_mol;
+    PWRITE(s_particle_mean)
+    double M_particle_mean = s_particle_mean / V_particle_mean;
+    PWRITE(M_particle_mean)
+
+    qDebug() << "--------------------------------";
 }
--- modelsimcore.cpp
+++ modelsimcore.cpp
@@ -4,7 +4,6 @@
 #include <QObject>
 #include <QThread>
 #include <QDebug>
-#include <QSettings>
 
 void Worker::doWork(const QString &parameter){
     //double i = 0;
@@ -26,54 +25,5 @@ ModelSimCore::ModelSimCore()
 
 void ModelSimCore::modelSimInit(void)
 {
-    qDebug() << "Fundamental constants init";
-    qDebug() << "--------------------------------";
-    qDebug() << "pi =" << pi;
-    qDebug() << "mu0 =" << mu0;
-    qDebug() << "Na =" << Na;
-    qDebug() << "kb =" << kb;
-    qDebug() << "R =" << R;
-
-    qDebug() << "";
-    qDebug() << "Experimental setup";
-    qDebug() << "--------------------------------";
-    QSettings parameters(QString("parameters.ini"), QSettings::IniFormat);
-
-    double Lx = parameters.value("Vessel/Lx", "0").toDouble();
-    double Ly = parameters.value("Vessel/Ly", "0").toDouble();
-    double Lz = parameters.value("Vessel/Lz", "0").toDouble();
-    qDebug() << "Lx =" << Lx;
-    qDebug() << "Ly =" << Ly;
-    qDebug() << "Lz =" << Lz;
-
-    double T = parameters.value("Fluid/T", "0").toDouble();
-    qDebug() << "T =" << T;
-
-    double B1 = parameters.value("Field/B1", "0").toDouble();
-    double B2 = parameters.value("Field/B2", "0").toDouble();
-    qDebug() << "B1 =" << B1;
-    qDebug() << "B2 =" << B2;
-
-    double d_mean = parameters.value("Fluid/d_mean", "0").toDouble();
-    qDebug() << "d_mean =" << d_mean;
-    double m_mol_rel = parameters.value("Nanoparticle/m_mol_rel", "0").toDouble();
-    qDebug() << "m_mol_rel =" << m_mol_rel;
-    double s_mol = parameters.value("Nanoparticle/s_mol", "0").toDouble();
-    qDebug() << "s_mol =" << s_mol;
-    double rho = parameters.value("Nanoparticle/rho", "0").toDouble();
-    qDebug() << "rho =" << rho;
-
-    qDebug() << "";
-    qDebug() << "Analysis";
-    qDebug() << "--------------------------------";
-
-    double V_particle = pi * pow(d_mean, 3) / 6.0;
-    double m_mean_temp = rho * V_particle;
-    double m_mol = m_mol_rel * 1E-3 / Na;
-
-    double s_particle = muB * s_mol * m_mean_temp / m_mol;
-    qDebug() << "s_particle =" << s_particle;
-    double M_particle = s_particle / V_particle;
-    qDebug() << "M_particle =" << M_particle;
-    qDebug() << "relation of M #1 =" << M_particle / 46000;
+    ModelParameters* modelParameters = new ModelParameters (QString("parameters.ini"));
 }
