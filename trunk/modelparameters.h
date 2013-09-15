ab6a526680351932187ce844b4cc9c5a085c7343
B.M.Tanygin@gmail.com <B.M.Tanygin@gmail.com@c73d5883-ad56-407f-ba61-18bf1b2862ed>
2013-09-15 19:26:24 +0000

    (Data) Parameters update (Tech) Refactoring of parameters processing (F) Added macroses for parameters processing and debug output

--- trunk/modelparameters.h
+++ trunk/modelparameters.h
@@ -2,6 +2,7 @@
 #define MODELPARAMETERS_H
 
 #include <math.h>
+#include <QString>
 
 extern const double pi;
 extern const double mu0;
@@ -10,10 +11,19 @@ extern const double Na;
 extern const double kb;
 extern const double R;
 
+#define PWRITE(P) par = #P; qDebug() << par + " = " + QString::number(##P, 'E');
+
+// reading the parameters from .ini file. "C" is a to-metric-system conversion factor.
+#define PREAD(O,P,C) obj = #O; par = #P; double (##P) = parameters.value(obj+"/"+par, "0").toDouble() * ##C;\
+    qDebug() << par + " = " + QString::number(##P, 'E');
+
 class ModelParameters
 {
 public:
-    ModelParameters();
+    ModelParameters(QString fileName);
+private:
+    QString obj; // physical object in the parameters .ini file
+    QString par; // paramater of physical object in the parameters .ini file
 };
 
 #endif // MODELPARAMETERS_H
