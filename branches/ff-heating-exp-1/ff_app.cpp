4ff2213ff139247f600e376b2bd8ed518a72c98b
B.M.Tanygin@gmail.com <B.M.Tanygin@gmail.com@c73d5883-ad56-407f-ba61-18bf1b2862ed>
2013-09-22 17:29:01 +0000

    (F) Branched from secondary_agg_experimental_2013_part_1: removed the secondary cluster related code.

---
+++ branches/ff-heating-exp-1/ff_app.cpp
@@ -0,0 +1,57 @@
+/**************************************************************************
+ * Copyright (C) 2011,2013 Dr. Bogdan Tanygin<b.m.tanygin@gmail.com>
+ * All rights reserved.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************/
+
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+#include "ff_app.h"
+
+#include "ff_sys_graphics.h"
+#include "ff_model_graphics.h"
+#include "ff_model.h"
+
+int main(int argc, char **argv)
+//int _tmain(int argc, _TCHAR* argv[])
+{
+    /*ff_gr_params_t *ff_gr_params;
+    ff_mgr_params_t *ff_mgr_params;
+    ff_model_params_t *ff_model_params;
+    
+    ff_gr_params = new ff_gr_params_t;
+    ff_mgr_params = new ff_mgr_params_t;
+    ff_model_params = new ff_model_params_t;*/
+    
+    // system graphics init
+    ff_gr_init(argc, argv);
+    
+    // model graphics init
+    ff_mgr_init();
+    
+    // physical model init
+    ff_model_init();
+    
+    // run graphics loop
+    ff_gr_loop();
+    
+    /*delete ff_gr_params;
+    delete ff_mgr_params;
+    delete ff_model_params;*/
+    
+    return 0;
+}
