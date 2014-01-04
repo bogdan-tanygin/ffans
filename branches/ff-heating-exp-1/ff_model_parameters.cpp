3adb515e8ccb121f671e40827517b3294d723c6e
B.M.Tanygin@gmail.com <B.M.Tanygin@gmail.com@c73d5883-ad56-407f-ba61-18bf1b2862ed>
2014-01-04 12:00:11 +0000

    (D) Parameters update

--- branches/ff-heating-exp-1/ff_model_parameters.cpp
+++ branches/ff-heating-exp-1/ff_model_parameters.cpp
@@ -34,7 +34,7 @@ const double kb = 1.3806488 * 1E-23; // [m2 kg s-2 K-1]
 
 // Space
 
-double scale = 7 * 0.13;// * 3.0;// / pow(50.0, 1 / 3.0);
+double scale = 7 * 0.15 * 3.0;// / pow(50.0, 1 / 3.0);
 double Lx = 1E-6 * scale, Ly = 1E-6 * scale, Lz = 1E-6 * scale; //meters
 
 //double kExtra = 0.27 * 20 * 1; // change only this coef. instead Lz
@@ -51,7 +51,7 @@ long slow_steps = 0;
 double smooth_r = 0.2;
 //double m_h_eff_tol = 1; // max. angle [rad] between m and B
 
-double T = 273.15 + 55; // K
+double T = 273.15 + 65; // K
 double kr = 1; // particle size parameter []
 //double R00 = 0.5 * 15E-9; // Radius of the nanoparticle [m]
 double delta = 2.0E-9;
@@ -59,10 +59,10 @@ double delta = 2.0E-9;
 //double Vself = (4 * pi / 3.0) * pow(R00, 3); // [m^3]
 
 // Parameters of oleic acid drop
-double R_oleic = (Lx / 8.0);
+double R_oleic = (Lx / 4.0);
 double eta_oleic = 25.6 * 1E-3; // [Pa * s]
 double sigma_sf = 32.5 * 1E-3; // [N / m]
-double sigma_sf_nano = sigma_sf * 1.0 * 5.017559E-04; // [N / m]
+double sigma_sf_nano = sigma_sf * 2.0 * 5.017559E-04; // [N / m]
 
 double rop = 5240; // mass density [kg / m^3]
 //double M0 = Vself * rop;  // mass [kg]
