d4cf68198695682014b271c03dca17b39626eae5
B.M.Tanygin@gmail.com <B.M.Tanygin@gmail.com@c73d5883-ad56-407f-ba61-18bf1b2862ed>
2014-02-13 20:59:28 +0000

    (D) Parameters update

--- branches/ff-heating-exp-1/tool_pack_bin_4.bat
+++ branches/ff-heating-exp-1/tool_pack_bin_4.bat
@@ -1,14 +1,14 @@
 mkdir pack_bin
 del /Q /S pack_bin\*.*
-mkdir pack_bin\1
-mkdir pack_bin\2
-mkdir pack_bin\3
-mkdir pack_bin\4
-copy Release\ff-heating-exp-1.exe pack_bin\1\
-copy Release\ff-heating-exp-1.exe pack_bin\2\
-copy Release\ff-heating-exp-1.exe pack_bin\3\
-copy Release\ff-heating-exp-1.exe pack_bin\4\
-copy *param* pack_bin\1\
-copy *param* pack_bin\2\
-copy *param* pack_bin\3\
-copy *param* pack_bin\4\
+mkdir pack_bin\50
+mkdir pack_bin\52
+mkdir pack_bin\54
+mkdir pack_bin\56
+copy Release\ff-heating-exp-1.exe pack_bin\50\
+copy Release\ff-heating-exp-1.exe pack_bin\52\
+copy Release\ff-heating-exp-1.exe pack_bin\54\
+copy Release\ff-heating-exp-1.exe pack_bin\56\
+copy *param* pack_bin\50\
+copy *param* pack_bin\52\
+copy *param* pack_bin\54\
+copy *param* pack_bin\56\
